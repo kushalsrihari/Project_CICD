4656eb91323a63fd14d7cc097c0d17f9f49116b9
kushalsrihari <76680565+kushalsrihari@users.noreply.github.com>
2023-10-21 09:40:50 +0530

    Part-7

---
+++ Terraform/variables.tf
@@ -0,0 +1,19 @@
+variable "vpc_config" {}
+
+variable "subnet_config" {}
+
+variable "internet_GW_cofig" {}
+
+variable "elastic_IP_config" {}
+
+variable "nat_GW_config" {}
+
+variable "route_table_config" {}
+
+variable "route_table_association_config" {}
+
+variable "region" {}
+
+variable "access_key" {}
+
+variable "secret_key" {}
