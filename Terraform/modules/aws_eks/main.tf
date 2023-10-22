18e933479564eb7d3535164cd96e04f7778cb619
kushalsrihari <76680565+kushalsrihari@users.noreply.github.com>
2023-10-22 08:56:50 +0530

    F-10

--- Terraform/modules/aws_eks/main.tf
+++ Terraform/modules/aws_eks/main.tf
@@ -16,20 +16,21 @@ resource "aws_eks_cluster" "example"{
 
 resource "aws_iam_role" "eks_cluster"{
     name = "eks_cluster"
+    
     assume_role_policy = <<POLICY
-    {
-        "Version": "2012-10-17",
-        Statement": [
-            {
-                "Effect": "Allow",
-                "Principal": {
-                    "Service": "eks.amazonaws.com"
-                },
-                "Action": "sts:AssumeRole"
-            }
-        ]
-    }
-    POLICY
+{
+    "Version": "2012-10-17",
+    Statement": [
+        {
+            "Effect": "Allow",
+            "Principal": {
+                "Service": "eks.amazonaws.com"
+            },
+            "Action": "sts:AssumeRole"
+        }
+    ]
+}
+POLICY
 }
 
 resource "aws_iam_role_policy_attachment" "AmzonEKSClusterPolicy" {
