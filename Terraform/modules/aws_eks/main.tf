cffa9e62ac0af22e7a3e85a18e91ce93445a8022
kushalsrihari <76680565+kushalsrihari@users.noreply.github.com>
2023-10-22 09:01:49 +0530

    f-12

--- Terraform/modules/aws_eks/main.tf
+++ Terraform/modules/aws_eks/main.tf
@@ -1,6 +1,6 @@
 resource "aws_eks_cluster" "example"{
     name = var.eks_cluster_name
-    role_arn = aws_iam_role.eks_role.arn
+    role_arn = aws_iam_role.eks_cluster.arn
 
     vpc_config {
         subnet_ids = var.subnet_ids
